7d8b9f9f3737113f6146869736ede1483d3fc8a6
Phu-Quy Felicia Trinh <35504528+feliciatrinh@users.noreply.github.com>
2019-04-04 14:40:28 -0700

    Create deleteMiddleNode.py

---
+++ Linked Lists/deleteMiddleNode.py
@@ -0,0 +1,8 @@
+# Delete a node in the middle of a singly linked list
+# input: a node from the linked lst, output: none
+def deleteMiddleNode(node):
+// Take over the next node
+  node.val = node.next.val
+  node.next = node.next.next
+  return
+  
