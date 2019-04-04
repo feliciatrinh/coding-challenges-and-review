de589afd6d2a527bb639233e05315d45d63b6b37
Phu-Quy Felicia Trinh <35504528+feliciatrinh@users.noreply.github.com>
2019-04-03 23:07:38 -0700

    Create palindromePermutation.py
    Given a string, write a function to check if it is a permutation of a palindrome.

---
+++ palindromePermutation.py
@@ -0,0 +1,23 @@
+from collections import Counter
+import re
+
+def pali(str): 
+    if len(str) == 1: 
+        return True
+    str = str.lower()
+    str = re.sub("\s", "", str)
+    letters = Counter()
+    for char in str: 
+        letters[char] += 1
+    odd_only = 0
+    for char in str: 
+        if letters[char] % 2 != 0: 
+            odd_only += 1
+    if odd_only > 1: 
+        return False
+    return True
+ 
+ print(pali("a") == True)
+ print(pali("racecar") == True)
+ print(pali("abd c") == False)
+ 
