4a43d5e37cae6789c2b3353c7e6575637b439980
Phu-Quy Felicia Trinh <35504528+feliciatrinh@users.noreply.github.com>
2019-04-05 12:58:34 -0700

    careful of how you choose to store values

---
+++ Arrays and Strings/hourGlassSum.py
@@ -0,0 +1,12 @@
+# Return an integer, the maximum hourglass sum in the array.
+def hourglassSum(arr):
+    sums = []
+    for row in range(len(arr)-2): 
+        for col in range(len(arr[0])-2): 
+            sum = 0
+            for j in range(col, col+3): 
+                sum += arr[row][j]
+                sum += arr[row+2][j]
+            sum += arr[row+1][col+1]
+            sums.append(sum)
+    return max(sums)
