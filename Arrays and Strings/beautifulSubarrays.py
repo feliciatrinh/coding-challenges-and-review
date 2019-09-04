4d4fe4d06933f6ae60d88741262c5a1d9f44201a
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2019-09-03 20:15:07 -0700

    num subarrays with m numbers satisfying some cond.

--- Arrays and Strings/beautifulSubarrays.py
+++ Arrays and Strings/beautifulSubarrays.py
@@ -1,22 +1,25 @@
-    # returns number of subarrays in a containing m odd numbers
-    def beautifulSubarrays(a, m):
+    # returns number of subarrays in arr containing m odd numbers
+    def beautifulSubarrays(arr, m):
     # Naive: creating all possible sub arrays then going through each one to see which
     # are beautiful would take too long
     # Better: when going through the array, keep track of num_subarrays in the part of the     
     # array where we haven't satisfied the num of odd elements yet 
     # so you can add it on later because each additional even element makes a different        
     # subarray
-    # keep track of the num of sub arrays where you meet the off num req and add it to the
+    # keep track of the num of sub arrays where you meet the odd num req and add it to the
     # above count
     num_subarrays = 0
     odd_elems = 0
-    subarrays_so_far = [0] * len(a)
-    for num in a: 
-        subarrays_so_far[odd_elems] += 1
+    subarrays_so_far = [0] * len(arr) 
+    # subarrays_so_far[i] is number of subarrays with i odd numbers
+    for num in arr: 
+        subarrays_so_far[odd_elems] += 1 
         if num % 2 != 0: # add to odd counter to move along the subarrays_so_far array
             odd_elems += 1
         if odd_elems >= m: # if you don't have enough odd nums
         # you don't increment the count
             num_subarrays += subarrays_so_far[odd_elems - m]
+            # if you have too many odd numbers, then you ignore the front portion of the array
+            # and keep forming subarrays in the later parts of the array
     return num_subarrays
     
