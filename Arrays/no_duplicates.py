61407a1b9fe8262fe7d5ea5d4d5fd1748636a53e
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2019-09-08 16:23:43 -0700

    return lst w/ no duplicates

---
+++ Arrays/no_duplicates.py
@@ -0,0 +1,25 @@
+def no_duplicates(lst):
+    """
+    Returns a (sorted) list with no duplicates. 
+    runtime O(n)
+    The list will be sorted because we used a set. 
+    Uses additional space for the set
+    """
+    no_dup = set()
+    for elem in lst:
+        no_dup.add(elem)
+    return list(no_dup)
+
+def no_duplicates_2(lst): 
+    """
+    Returns a list with no duplicates. Maintains the original order.
+    Runtime O(n)
+    Uses additional space for a new list and a set
+    """
+    no_dup = set()
+    new_lst = []
+    for elem in lst: 
+        if elem not in no_dup: 
+            no_dup.add(elem)
+            new_lst.append(elem)
+    return new_lst
