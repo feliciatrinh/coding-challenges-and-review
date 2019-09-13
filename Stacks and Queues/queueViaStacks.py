f66f3c286305563aac2f84cc857def8337f177cf
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2019-09-13 12:47:13 -0700

    implement queue class using stacks

--- Stacks and Queues/queueViaStacks.py
+++ Stacks and Queues/queueViaStacks.py
@@ -1,24 +1,43 @@
-# Implement a MyQueue class which implements a queue using two stacks
+"""
+Implement a MyQueue class which implements a queue using two stacks. 
+
+Note: python's collections.deque class is a double-ended queue
+(implemented as a doubly-linked list) that supports fast O(1) enqueuing and
+dequeuing from either end. 
+Ex)
+from collections import deque
+q = deque()
+q.append("one")
+q.append("two")
+q.append("three")
+
+q.popleft()  # returns "one"
+q.popright()  # returns "three"
+
+Can serve as either a queue or a stack.
+"""
+
+
 class MyQueue(): 
     def __init__(self): 
         self.old = Stack()
         self.new = Stack()
         
-    def add(self, item): 
+    def add(self, item):
         self.new.push(item)
     
-    def remove(self): 
+    def remove(self):
         self.fillOld()
         return self.old.pop()
     
-    def peek(self): 
+    def peek(self):
         self.fillOld()
         return self.old.peek()
     
-    def isEmpty(self): 
-        return (self.old.isEmpty() & self.new.isEmpty())
+    def isEmpty(self):
+        return self.old.isEmpty() & self.new.isEmpty()
         
-    def fillOld(self): 
+    def fillOld(self):
         if self.old.isEmpty(): 
             while not self.new.isEmpty(): 
                 self.old.push(self.new.pop())
@@ -26,22 +45,22 @@ class MyQueue():
 class Stack(): 
     def __init__(self): 
         self.array = []
-        self.length = 0
+        self.size = 0
     
-    def push(self, item): 
+    def push(self, item):
         self.array.append(item)
-        self.length += 1
+        self.size += 1
     
-    def pop(self): 
+    def pop(self):
         if not self.isEmpty(): 
-            self.length -= 1
+            self.size -= 1
             return self.array.pop()
     
-    def peek(self): 
-        return self.array[self.length - 1]
+    def peek(self):
+        return self.array[self.size - 1]
     
-    def isEmpty(self): 
-        return self.length == 0
+    def isEmpty(self):
+        return self.size == 0
         
 Q = MyQueue()
 Q.add(3)
