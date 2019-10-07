facf0e69eae2efae8d1dfdaae016ebf9a4650a85
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2019-10-07 13:04:10 -0700

    clearer comments

--- arrays/arrayManipulation.py
+++ arrays/arrayManipulation.py
@@ -1,12 +1,32 @@
-# Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each of the arr element between
-# two given indices, inclusive. Once all operations have been performed, return the max value in your array.
-# runtime O(num operations + length array)? 
-# solution achieved after reading discussion board in hackerrank
+"""
+Input: 2D array of operations
+Output: Maximum value in array as a long int
+
+Starting with a 1-indexed array of zeros and a list of operations, 
+for each operation add a value to each of the arr elements between
+two given indices, inclusive. Once all operations have been performed, 
+return the max value in your array.
+
+Runtime O(num operations + length array)
+
+example
+a  b  k
+1  2  10
+2  3  15
+3  4  2
+1  6  14
+5  6  3
+
+returns 39
+"""
+
 
 def arrayManipulation(n, queries):
     arr = [0]*n
     for row in queries:
         left = row[0] - 1
+        # you don't subtract 1 from right because this index is 1 spot after b
+        # the value at this index represents the difference between the current element and the previous element
         right = row[1]
         k = row[2]
         arr[left] += k
@@ -15,7 +35,8 @@ def arrayManipulation(n, queries):
             arr[right] -= k
     maximum = 0
     diffs = 0
-    # add up the differences, maximum is the sum of the positive differences
+    # add up the differences from left to right
+    # solution is the maximum running sum you achieve
     for item in arr:
         diffs += item
         if maximum < diffs: 
