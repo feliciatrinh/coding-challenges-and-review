a8767cd7bae6fac34212494bb1091fd9743fb4eb
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2019-10-28 00:18:41 -0700

    typo

--- linked lists/deleteMiddleNode.py
+++ linked lists/deleteMiddleNode.py
@@ -1,7 +1,7 @@
 # Delete a node in the middle of a singly linked list
 # input: a node from the linked lst, output: none
 def deleteMiddleNode(node):
-// Take over the next node
+# Take over the next node
   node.val = node.next.val
   node.next = node.next.next
   return
