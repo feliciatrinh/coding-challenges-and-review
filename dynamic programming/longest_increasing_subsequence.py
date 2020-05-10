f02ded6aafe3fd025282a2112d88dbb19795d5f2
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2020-05-09 23:22:18 -0700

    added dynamic programming approaches

--- dynamic programming/longest_increasing_subsequence.py
+++ dynamic programming/longest_increasing_subsequence.py
@@ -1,1 +1,83 @@
+"""
+Input: an array A of integers
+Output: longest increasing subsequence (LIS) in terms of length
 
+Example 1:
+Input: [11, 100, 1, 7, 18, 23, 25, 10]
+Output: [1, 7, 18, 23, 25]
+
+Idea:
+- Can create a DAG out of these with edges i -> j if A[i] < A[j]
+- The longest increasing subsequence is the longest path in this DAG
+    - To solve longest path in a DAG, we find the longest path that ends at
+      each node starting with the first node
+
+Subproblem:
+- L(i) is the length of the longest increasing subsequence that ends at index i
+
+Recurrence relation:
+- L(i) = max {L(j) + 1, L(i)} for all j -> i edges which are incoming edges
+  into node i
+
+Algorithm:
+- Solve in increasing order of i
+- Keep a prev array where prev[i] = j for which the maximum length is attained
+- last index of the LIS (called last_index) is argmax{L(1), L(2), ..., L(n)}
+- step backwards through prev array starting from last_index, these are the
+  indices of the LIS
+
+Runtime: O(n^2) b/c runtime is the number of edges
+But a O(nlogn) solution exists.
+"""
+
+
+def longest_increasing_subsequence(arr):
+    def construct_DAG(arr):
+        """
+        Construct a dictionary adjacency list representation of the DAG
+        There's an edge j -> i is A[j] < A[i]
+        Runtime: O(n^2) for number of edges
+        """
+        graph = {i: [] for i in range(len(arr))}
+        for i in range(len(arr)):
+            for j in range(0, i):
+                if arr[j] < arr[i]:
+                    graph[i].append(j)
+        return graph
+
+    if len(arr) < 2:
+        return arr
+
+    graph = construct_DAG(arr)
+    # contains length of LIS ending at index i
+    L = [1] * len(arr)
+
+    # prev[i] = j for which the maximum length is attained
+    prev = [-1] * len(arr)
+    # keep track of the global maximum so we can keep track of the last node
+    longest_len = 1
+    last_index = 0
+    for i in range(len(arr)):
+        for j in graph[i]:
+            # L(i) = max {L(j) + 1, L(i)} for all j -> i
+            if L[j] + 1 > L[i]:
+                L[i] = L[j] + 1
+                prev[i] = j
+            if L[i] > longest_len:
+                longest_len = L[i]
+                last_index = i
+
+    # step backwards through prev starting from the index of the last element
+    # in the LIS
+    result = []
+    while last_index != -1:
+        result.insert(0, arr[last_index])
+        last_index = prev[last_index]
+    return result
+
+
+assert longest_increasing_subsequence([11, 100, 1, 7, 18, 23, 25, 10]) == [1, 7, 18, 23, 25]
+assert longest_increasing_subsequence([2, 3, 4, -3, -5, 100]) == [2, 3, 4, 100]
+assert longest_increasing_subsequence([1]) == [1]
+assert longest_increasing_subsequence([1, -4]) == [1]
+assert longest_increasing_subsequence([10,9,2,5,3,7,101,18]) == [2, 5, 7, 101]
--- strings/longestPalindromicSubstring.py
+++ strings/longestPalindromicSubstring.py
@@ -3,7 +3,7 @@ Source: LeetCode
 Input: string s
 Output: longest palindromic substring in s
 
-Runtime: IS TOO SLOW
+Runtime: O(n^2)
 
 Example 1:
 Input: "babad"
@@ -14,28 +14,151 @@ Example 2:
 Input: "cbbd"
 Output: "bb"
 """
+def expandAroundCenter(s: str) -> str:
+    """
+    Source: leetcode
+    Runtime: O(n^2), Space complexity: O(1)
 
-# consider using a generator instead?
+    Idea: 
+    - palindromes mirror around its center, so palindromes can be expanded
+      from its center
+    - each string has 2n - 1 possible centers (start from the first letter,
+      include the spaces in between letters as possible centers, stop at the
+      last letter)
 
-def longestPalindrome(s: str) -> str:
+    - Start from a center, look at the letter to the left and the letter to the
+      right. If they are the same, then keep expanding outward.
+
+    - Use left and right pointers to keep track of where the center is 
+    - left and right pointers begin at index 0. Left and right can either point
+      to the same letter, or right can be left + 1
+    """
+    def expand(s, left, right):
+        """
+        Expands from the center and returns the length of the palindromic
+        substring that is found.
+        """
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        # subtract 1 b/c you expanded one extra before the while loop ended
+        return right - left - 1
+
     def isPalindrome(s):
         """ Runtime: O(n) """
         if len(s) < 2:
             return True
-        if s[0] != s[-1]:
-            return False
-        return isPalindrome(s[1:-1])
+        if s[0] == s[-1]:
+            return isPalindrome(s[1:-1])
+        return False
+
+    # also takes care of when s is the empty string
+    if isPalindrome(s):
+        return s
+
+    max_left = 0
+    max_right = 0
+    for i in range(len(s)):
+        # for the center at each letter
+        letter_len = expand(s, i, i)
+        # for the center b/w two letters
+        space_len = expand(s, i, i + 1)
+        curr_len = max(letter_len, space_len)
+        if curr_len > max_right - max_left + 1:
+            max_left = i - (curr_len - 1) // 2
+            max_right = i + curr_len // 2
+
+    return s[max_left: max_right + 1]
+
+
+def dynamicLongestPalindrome(s: str) -> str:
+    """
+    Dynamic programming solution
+    Source: leetcode
+    Runtime: O(n^2), Space complexity: O(n^2)
+
+    Subproblem: P(i, j), boolean True if substring between i and j inclusive
+                aka s[i:j + 1] is a palindrome
+    Base Cases: P(i, i) == True
+                P(i, i + 1) = s[i] == s[i + 1]
+    Recurrence Relation:
+                P(i, j) = ( P(i + 1, j - 1) and (s[i] == s[j]) )
+    
+    - we already look at 2 letter substrings in the second base case, so we
+      then look at 3 letter substrings, then 4 letter substrings and so on.
+    - if the inside substring is a palindrome, then the entire string is a
+      palindrome iff s[i] == s[j]
+        - Ex: in "ababa", "bab" is a palindrome so "ababa" is a palindrome b/c
+          the first and last letter are the same
+    """
+    def isPalindrome(s):
+        """ Runtime: O(n) """
+        if len(s) < 2:
+            return True
+        if s[0] == s[-1]:
+            return isPalindrome(s[1:-1])
+        return False
+
+    # also takes care of when s is the empty string
+    if isPalindrome(s):
+        return s
+
+    longest_pali = s[0]
+    # initialize palindrome table
+    p = [[False] * len(s) for _ in range(len(s))]
+    # base cases
+    for i, s_i in enumerate(s):
+        for j, s_j in enumerate(s[i:], i):
+            if i == j:
+                p[i][j] = True
+            if j == i + 1:
+                p[i][j] = s_i == s_j
+            if p[i][j]:
+                if j - i + 1 > len(longest_pali):
+                    longest_pali = s[i: j + 1]
+
+    # we start looking for 3 letter substrings: 3-letter substrings' indices
+    # differ by 2 so we start at 2
+    for diff in range(2, len(s)):
+        for i in range(len(s) - diff):
+            p[i][i + diff] = p[i + 1][i + diff - 1] and s[i] == s[i + diff]
+            if p[i][i + diff]:
+                # the current palindromic substring has length diff + 1
+                if diff + 1 > len(longest_pali):
+                    longest_pali = s[i: i + diff + 1]
+
+    return longest_pali
+
+
+def bruteForceLongestPalindrome(s: str) -> str:
+    """
+    Brute force solution is to pick all possible starting and ending positions
+    for a substring and verify if it's a palindrome.
+    Runtime: O(n^3)
+    """
+    def isPalindrome(s):
+        """ Runtime: O(n) """
+        if len(s) < 2:
+            return True
+        if s[0] == s[-1]:
+            return isPalindrome(s[1:-1])
+        return False
 
     def substrings(s):
         """ Runtime: O(n^2) """
         from itertools import combinations
-        return [s[i:j] for i, j in combinations(range(len(s) + 1), 2)]
+        return (s[i:j] for i, j in combinations(range(len(s) + 1), 2))
 
     if isPalindrome(s):
         return s
 
     # sorted() runtime is O(nlog(n))
     substrs = sorted(substrings(s), key=len, reverse=True)[1:]
-    for sub in substrs:
+    for sub in substrings(s):
         if isPalindrome(sub):
             return sub
+
+assert expandAroundCenter("aaaa") == "aaaa"
+assert expandAroundCenter("babad") == "bab"
+assert expandAroundCenter("cbbd") == "bb"
+assert expandAroundCenter("abacab") == "bacab"
