958d9810e63c1e2de3cfc6f1c816994cee1e4ee1
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2020-05-28 00:29:56 -0700

    added soln to slightly different problem statement

--- arrays/three_sum.py
+++ arrays/three_sum.py
@@ -64,6 +64,64 @@ def three_sum(nums):
     return list(triplets for triplets, _ in groupby(triplets))
 
 
+def two_sum_alt(nums, target):
+    """
+    Given an array of integers, return 2 integers in nums that sum to target and None otherwise.
+    """
+    if not nums:
+        return None
+
+    have_seen = {nums[0]}
+    for num in nums[1:]:
+        complement = target - num
+        if complement in have_seen:
+            return complement, num
+        have_seen.add(num)
+
+
+def two_sum_alt_recursive(nums, target, have_seen):
+    """
+    Given an array of integers, return 2 integers in nums that sum to target and None otherwise.
+    """
+    if not nums:
+        return None
+
+    num = nums[0]
+    complement = target - num
+    if complement in have_seen:
+        return complement, num
+
+    have_seen.add(num)
+    return two_sum_alt_recursive(nums[1:], target, have_seen)
+
+
+def three_sum_alt(nums, target):
+    """
+    Given an array of integers, return 3 integers in nums that sum to target.
+    Runtime: O(n^2)
+    """
+    for i, num in enumerate(nums):
+        two_sum_result = two_sum_alt(nums[:i] + nums[i + 1:], target - num)
+        if two_sum_result is not None:
+            a, b = two_sum_result
+            return num, a, b
+
+
+def three_sum_alt_recursive(nums, target, index):
+    """
+    Given an array of integers, return 3 integers in nums that sum to target.
+    Runtime: O(n^2)
+    """
+    if index > len(nums) - 1:
+        return None
+
+    two_sum_result = two_sum_alt_recursive(nums[:index] + nums[index + 1:], target - nums[index], set())
+    if two_sum_result is None:
+        return three_sum_alt_recursive(nums, target, index + 1)
+    a, b = two_sum_result
+    return nums[index], a, b
+
+
 assert three_sum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
 assert three_sum([0, 0, 0, 0, 0, 0]) == [[0, 0, 0]]
 assert three_sum([-5, 5, 9]) == []
@@ -71,3 +129,9 @@ assert three_sum([4, -4]) == []
 assert three_sum([3, 0, -2, -1, 1, 2]) == [[-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
 assert three_sum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]) == [[-4, -2, 6], [-4, 0, 4], [-4, 1, 3], [-4, 2, 2],
                                                                         [-2, -2, 4], [-2, 0, 2]]
+
+assert three_sum_alt([-1, 0, 1, 2, -1, -4], 0) == (-1, 0, 1)
+assert three_sum_alt([-5, 5, 9], 0) is None
+
+assert three_sum_alt_recursive([-1, 0, 1, 2, -1, -4], 4, 0) is None
+assert three_sum_alt_recursive([8, 1, -2, 5, 6], 12, 0) == (8, -2, 6)
--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -8,14 +8,46 @@ Runtime O(n)
 
 
 def two_sum(nums, target):
-	"""
-	One pass hash table implementation. Check if the complement is in the dictionary
-	and return immediately if it is. Otherwise, add the current element to the dictionary.
-	Dictionary key, value is number, index.
-	"""
-	ints = {}
-	for i in range(len(nums)):
-		complement = target - nums[i]
-		if complement in ints:
-			return [ints[complement], i]
-		ints[nums[i]] = i
+    """
+    One pass hash table implementation. Check if the complement is in the dictionary
+    and return immediately if it is. Otherwise, add the current element to the dictionary.
+    Dictionary key, value is number, index.
+    """
+    ints = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in ints:
+            return [ints[complement], i]
+        ints[nums[i]] = i
+
+
+def two_sum_alt(nums, target):
+    """
+    :return: two integers in nums that sum to target
+    """
+    have_seen = {nums[0]}
+    for num in nums[1:]:
+        complement = target - num
+        if complement in have_seen:
+            return complement, num
+        have_seen.add(complement)
+
+
+def two_sum_alt_recursive(nums, target, have_seen):
+    """
+    Given an array of integers, return 2 integers in nums that sum to target and None otherwise.
+    """
+    if not nums:
+        return None
+
+    num = nums[0]
+    complement = target - num
+    if complement in have_seen:
+        return complement, num
+
+    have_seen.add(num)
+    return two_sum_alt_recursive(nums[1:], target, have_seen)
+
+
+assert two_sum_alt([-3, 0, 6], 3) == (-3, 6)
+assert two_sum_alt([1, 2, 3, 4], 10) is None
