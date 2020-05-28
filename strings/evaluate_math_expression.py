b42f3702e13dce56fed39f447df41d5759fb8652
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2020-05-27 23:32:03 -0700

    improved solution

--- strings/evaluate_math_expression.py
+++ strings/evaluate_math_expression.py
@@ -15,7 +15,23 @@ Output: 4
 TODO: solve expression containing * and / as well
 """
 
+
+def eval_add_sub_better(expression):
+    """
+    Treat subtractions as negative integers and additions as positive integers then sum all of the integers together.
+    """
+    import re
+
+    expression = expression.replace(" ", "")
+    nums_str = re.findall(r'\-?\d+', expression)
+    nums = map(int, nums_str)
+    return sum(nums)
+
+
 def eval_add_sub(expression):
+    """
+    Old solution
+    """
     expression = expression.replace(" ", "")
     result = int(expression[0])
     for i in range(1, len(expression), 2):
@@ -31,3 +47,9 @@ assert eval_add_sub("3+2-1") == 4
 assert eval_add_sub("0 - 4 - 2") == -6
 assert eval_add_sub("0 -4- 2") == -6
 assert eval_add_sub("7") == 7
+
+assert eval_add_sub_better("3+2-1") == 4
+assert eval_add_sub_better("0 - 4 - 2") == -6
+assert eval_add_sub_better("0 -4- 2") == -6
+assert eval_add_sub_better("7") == 7
+assert eval_add_sub_better("-7-9+11") == -5
