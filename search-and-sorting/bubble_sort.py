9a25b293e0fcaa2fc41816413c4ad13f656d542b
Phu-Quy Felicia Trinh <felicia.trinh@gmail.com>
2020-08-09 19:39:37 -0700

    small optimization

--- search-and-sorting/bubble_sort.py
+++ search-and-sorting/bubble_sort.py
@@ -12,7 +12,8 @@ Idea
 
 def bubble_sort(lst):
     for i in range(0, len(lst) - 1):
-        for j in range(1, len(lst)):
+        # the last i elements are already sorted
+        for j in range(1, len(lst) - i):
             if lst[j - 1] > lst[j]:
                 lst[j - 1], lst[j] = lst[j], lst[j - 1]
 
